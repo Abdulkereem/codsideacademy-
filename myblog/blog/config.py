09f262c5c4fcbb3e8e3f4453669b9d29af425877
Cyberhero <Abdulkereemokereem@gmail.com>
2018-09-01 18:20:17 +0100

    Update config.py

--- myblog/blog/config.py
+++ myblog/blog/config.py
@@ -1,8 +1,4 @@
-userpass = 'mysql://root:@'
-basedir = '127.0.0.1'
 
-#basedir = '192.168.1.139'
-dbname = '/computer'
 
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///tmp/blog.db' #userpass + basedir + dbname 
@@ -15,19 +11,10 @@ WHOOSH_BASE = 'whoosh'
 
 
 MAIL_SERVER='smtp.gmail.com'
-MAIL_USERNAME='codesideacademy@gmail.com'
-MAIL_PASSWORD='Abdulkereem22'
-MAIL_DEFAULT_SENDER = 'CodeSide Academy'
+MAIL_USERNAME='username@email.com'
+MAIL_PASSWORD='password'
+MAIL_DEFAULT_SENDER = 'Your naae'
 MAIL_PORT=465
 MAIL_USE_SSL=True
 MAIL_USE_TLS=False
 
-
-#userpass = 'mysql://root:@'
-#basedir = '127.0.0.1'
-
-#basedir = '192.168.1.139'
-#dbname = '/locker'
-
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////temp/test.db'#userpass + basedir + dbname  #userpass + basedir + dbname #
